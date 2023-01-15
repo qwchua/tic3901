8998433ea4db7bc85ec6af31138d60f5d621e728
qwchua <chuaqiwang@gmail.com>
2023-01-16 07:38:27 +0800

    add new test case

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,8 +5,26 @@ import subprocess
 def test_parseGitLogToNumOfCommits():
     gitlogcommand = "git log index.html"
     unparsedlog = subprocess.run(
-        gitlogcommand, shell=True, cwd="test_repos/2048", capture_output=True, text=True
+        gitlogcommand,
+        shell=True,
+        cwd="repos-for-testing/2048",
+        capture_output=True,
+        text=True,
     )
     unparsedlog = unparsedlog.stdout
     total = parseGitLogToNumOfCommits(unparsedlog)
     assert total == 284
+
+
+def test_parseGitLogToNumOfCommits():
+    gitlogcommand = "git log tox.ini"
+    unparsedlog = subprocess.run(
+        gitlogcommand,
+        shell=True,
+        cwd="repos-for-testing/flask",
+        capture_output=True,
+        text=True,
+    )
+    unparsedlog = unparsedlog.stdout
+    total = parseGitLogToNumOfCommits(unparsedlog)
+    assert total == 595
