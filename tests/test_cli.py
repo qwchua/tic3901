2f90db8df992909bd757e818218225254a65b2fd
drrnsyk <darren.syk@gmail.com>
2023-05-01 00:39:36 +0800

    added test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,3 @@
-import pytest
 from click.testing import CliRunner
 from gitpraise.cli import run_command
 import os
@@ -8,5 +7,9 @@ def test_run_command():
     os.chdir('repos-for-testing/cli_test_repo')
     result = runner.invoke(run_command, ["--repotype", "git", "--path", "clifile.txt", "--outputformat", "txt", "--sigchange", "50", "--ref", "master"], catch_exceptions=True)
     assert result.exit_code == 0
-    expected_output = "clifile.txt\n 33b39a597 drrnsyk 2023-04-30 00:50:31+08:00 1)  Hello, world"
-    assert expected_output == result.output.strip()
+    expected_output = "clifile.txt\n 33b39a597  drrnsyk 2023-04-30 00:50:31+08:00 1)  Hello, world"
+    
+    with open('output.txt', 'r') as f:
+        output = f.read().strip()
+    
+    assert expected_output == output
