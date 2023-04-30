b3e7a4ed306a55766c9242a5cd8015f2246fbb46
qwchua <chuaqiwang@gmail.com>
2023-05-01 01:54:51 +0800

    removed since option from cli and updated integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,12 +1,12 @@
-# from gitpraise.database import *
-# from gitpraise.analyzer import *
-# from gitpraise.datavisualization import *
-# from gitpraise.scanner import *
-# from tqdm import tqdm
-# import os
-# import cProfile
-# import pstats
-# import concurrent.futures
+from gitpraise.database import *
+from gitpraise.analyzer import *
+from gitpraise.datavisualization import *
+from gitpraise.scanner import *
+from tqdm import tqdm
+import os
+import cProfile
+import pstats
+import concurrent.futures
 
 
 # # multithreaded experimental
@@ -60,99 +60,97 @@
 #     results.sort_stats(pstats.SortKey.TIME)
 #     results.print_stats(15)
 
-# # def test_integration_all_except_cli_singlethreaded():
-# #     with cProfile.Profile() as profile:
-# #         os.chdir('repos-for-testing/flask')
+# def test_integration_all_except_cli_singlethreaded():
+#     with cProfile.Profile() as profile:
+#         os.chdir('repos-for-testing/flask')
 
-# #         #from CLI retrieve this
-# #         repotype = "git"
-# #         path = "ALL"
-# #         since = None
-# #         outputformat = "txt"
-# #         significantchangepercentage = 0
-# #         detectRename = True
-# #         ref = "main"
+#         #from CLI retrieve this
+#         repotype = "git"
+#         path = "ALL"
+#         since = None
+#         outputformat = "txt"
+#         significantchangepercentage = 0
+#         detectRename = True
+#         ref = "main"
 
-# #         scanner = Scanner(repotype)
-# #         filesToProcess = scanner.findFiles(path)
+#         scanner = Scanner(repotype)
+#         filesToProcess = scanner.findFiles(path)
 
-# #         results = []
-# #         failed = []
+#         results = []
+#         failed = []
 
-# #         for file in tqdm(filesToProcess):
-# #             try:
-# #                 databaseBuilder = DatabaseBuilder()
-# #                 databaseBuilder.setRepoType(repotype)
-# #                 databaseBuilder.setFileName(file)
-# #                 databaseBuilder.setSince(since)
-# #                 databaseBuilder.setRef(ref)
-# #                 databaseBuilder.setDetectRenames(True)
+#         for file in tqdm(filesToProcess):
+#             try:
+#                 databaseBuilder = DatabaseBuilder()
+#                 databaseBuilder.setRepoType(repotype)
+#                 databaseBuilder.setFileName(file)
+#                 databaseBuilder.setSince(since)
+#                 databaseBuilder.setRef(ref)
+#                 databaseBuilder.setDetectRenames(True)
 
-# #                 db = databaseBuilder.build()
+#                 db = databaseBuilder.build()
 
-# #                 analyzer = Analyzer(db)
-# #                 result = analyzer.getLinesContributions(significantchangepercentage)
-# #                 results.append(result)
+#                 analyzer = Analyzer(db)
+#                 result = analyzer.getLinesContributions(significantchangepercentage)
+#                 results.append(result)
 
-# #             except MergeError as me: print(me,file)
+#             except MergeError as me: print(me,file)
 
-# #             except:
-# #                 failed.append(file)
+#             except:
+#                 failed.append(file)
 
-# #         for f in failed:
-# #             print("FAILED")
-# #             print(f)
+#         for f in failed:
+#             print("FAILED")
+#             print(f)
 
-# #     results = pstats.Stats(profile)
-# #     results.sort_stats(pstats.SortKey.TIME)
-# #     results.print_stats(15)
+#     results = pstats.Stats(profile)
+#     results.sort_stats(pstats.SortKey.TIME)
+#     results.print_stats(15)
     
 
-# # def test_integration_all_except_cli_singlethreaded():
-# #     os.chdir('repos-for-testing/nanoGPT')
+def test_integration_all_except_cli_singlethreaded():
+    os.chdir('repos-for-testing/2048')
 
-# #     #from CLI retrieve this
-# #     repotype = "git"
-# #     path = "ALL"
-# #     since = None
-# #     outputformat = "csv"
-# #     significantchangepercentage = 0
-# #     detectRename = True
-# #     ref = "master"
+    #from CLI retrieve this
+    repotype = "git"
+    path = "ALL"
+    outputformat = "txt"
+    significantchangepercentage = 0
+    detectRename = True
+    ref = "master"
 
-# #     scanner = Scanner(repotype)
-# #     filesToProcess = scanner.findFiles(path)
+    scanner = Scanner(repotype)
+    filesToProcess = scanner.findFiles(path)
     
-# #     results = []
-# #     failed = []
+    results = []
+    failed = []
 
-# #     for file in tqdm(filesToProcess):
-# #         try:
-# #             databaseBuilder = DatabaseBuilder()
-# #             databaseBuilder.setRepoType(repotype)
-# #             databaseBuilder.setFileName(file)
-# #             databaseBuilder.setSince(since)
-# #             databaseBuilder.setRef(ref)
-# #             databaseBuilder.setDetectRenames(True)
+    for file in tqdm(filesToProcess):
+        try:
+            databaseBuilder = DatabaseBuilder()
+            databaseBuilder.setRepoType(repotype)
+            databaseBuilder.setFileName(file)
+            databaseBuilder.setRef(ref)
+            databaseBuilder.setDetectRenames(True)
 
-# #             db = databaseBuilder.build()
+            db = databaseBuilder.build()
 
-# #             analyzer = Analyzer(db)
-# #             result = analyzer.getLinesContributions(significantchangepercentage)
-# #             results.append(result)
+            analyzer = Analyzer(db)
+            result = analyzer.getLinesContributions(significantchangepercentage)
+            results.append(result)
 
-# #         except MergeError as me: print(me,file)
+        except MergeError as me: print(me,file)
 
-# #         except:
-# #             failed.append(file)
+        except:
+            failed.append(file)
 
-# #     for f in failed:
-# #         print("FAILED")
-# #         print(f)
+    for f in failed:
+        print("FAILED")
+        print(f)
 
 
-# #     dv = DataVisualization()
-# #     dv.process(results, outputformat)
+    dv = DataVisualization()
+    dv.process(results, outputformat)
 
 
 # # def test_integration_all_except_cli_single_file():
